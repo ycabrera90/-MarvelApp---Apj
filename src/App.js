369d3fa46da5c9c8eb49bd7a81863b86cd47b709
yosniel <yosniel.ch@gmail.com>
2022-10-18 14:04:44 -0300

    some adjustment were made to deploy the app on server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { useEffect } from "react";
 import "./App.css";
-
+import React, { Suspense } from "react";
 import { Route, Redirect } from "react-router-dom";
-import MainHeader from "./components/MainHeader/MainHeader";
-import InfoBar from "./components/InfoBar/InfoBar";
-import HomePage from "./pages/HomePage";
+
+// for dev env.
+// import MainHeader from "./components/MainHeader/MainHeader";
+// import InfoBar from "./components/InfoBar/InfoBar";
+// import HomePage from "./pages/HomePage";
+
+// deploy on Server
+const MainHeader = React.lazy(() =>
+  import("./components/MainHeader/MainHeader")
+);
+const InfoBar = React.lazy(() => import("./components/InfoBar/InfoBar"));
+const HomePage = React.lazy(() => import("./pages/HomePage"));
 
 function App() {
   return (
-    <>
+    <Suspense fallback={<></>}>
       <MainHeader />
       <InfoBar />
 
@@ -19,7 +27,7 @@ function App() {
       <Route path="/series" exact>
         <HomePage />
       </Route>
-    </>
+    </Suspense>
   );
 }
 
